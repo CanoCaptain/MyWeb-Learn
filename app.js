fa0052f52d2449e7a3b00816b7c21d63be7c1dde
A Fang <hunglexuan92@live.com>
2022-07-15 20:14:35 +0700

    HungXoan

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const path = require("path");
+const app = express();
+
+
+app.use("/", express.static(path.join(__dirname, "./build")));
+
+app.get('*', (req, res)=>
+{
+    res.sendFile(path.join(__dirname, './build/index.html'))
+})
+
+
+
+app.listen(process.env.port, ()=> {
+    console.log("Listening ok");
+  });
+
+
+
+
+
